2a383506842135b5d1af9901bd7de192f82cb662
unknown <smtishkar@mail.ru>
2023-07-27 18:35:20 +0300

    Реализована задача 2

--- task2.py
+++ task2.py
@@ -1,9 +1,19 @@
-# Тут какой-то тескт
-
-#dgfsdgdfg
-# sdgsdgsd
-# sdgsdgsd
+# Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь, где ключ — значение переданного аргумента, 
+# а значение — имя аргумента. Если ключ не хешируем, используйте его строковое представление. 
+# Пример: rev_kwargs(res=1, reverse=[1, 2, 3]) -> {1: 'res', '[1, 2, 3]': 'reverse'}
 
 
-matrix = [[1,2,3],[4,5,6]]
-print(matrix)
+def reverse_dict(**kwargs):
+    result = {}
+    for key,value in kwargs.items():
+        try: 
+            hash(value)
+            result[value] = key
+        except:
+             result[str(value)] = key
+    return result
+
+
+
+print(reverse_dict(res=1, reverse=[1, 2, 3]))
+print(reverse_dict(res='3434', reverse='434'))
